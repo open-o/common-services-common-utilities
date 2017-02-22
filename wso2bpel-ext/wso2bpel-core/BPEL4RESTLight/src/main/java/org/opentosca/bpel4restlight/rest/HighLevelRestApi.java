369623cc4b5c293afc8f217513302084c0fa094c
qinlihan <qin.lihan@zte.com.cn>
2017-02-22 16:05:01 +0800

    modify license header
    Change-Id: I2e00b1f0a4a203758e00eb8a4276be885088015a
    Issue-id: TOSCA-228
    Signed-off-by: qinlihan <qin.lihan@zte.com.cn>
    
    Former-commit-id: 9a2ab1d97796d48ad5f1d5a7135a601cb6f6bbed

--- wso2bpel-ext/wso2bpel-core/BPEL4RESTLight/src/main/java/org/opentosca/bpel4restlight/rest/HighLevelRestApi.java
+++ wso2bpel-ext/wso2bpel-core/BPEL4RESTLight/src/main/java/org/opentosca/bpel4restlight/rest/HighLevelRestApi.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2013 University of Stuttgart.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and the Apache License 2.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v10.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ *******************************************************************************/
+/*
+ * Modifications Copyright 2016-2017 ZTE Corporation.
+ */
 /**
  * This class wraps HTTP-Method functionality and thereby abstracts from low
  * level code to simplify the usage.
--- wso2bpel-ext/wso2bpel-core/BPEL4RESTLight/src/main/java/org/opentosca/bpel4restlight/rest/LowLevelRestApi.java
+++ wso2bpel-ext/wso2bpel-core/BPEL4RESTLight/src/main/java/org/opentosca/bpel4restlight/rest/LowLevelRestApi.java
@@ -1,4 +1,15 @@
-/**
+/*******************************************************************************
+ * Copyright (c) 2012-2013 University of Stuttgart.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and the Apache License 2.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v10.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ *******************************************************************************/
+/*
+ * Modifications Copyright 2016-2017 ZTE Corporation.
+ */
+ /**
  * This static-class eases HTTP-method execution by self-managed fault-handling
  * and automated Response-information processing
  */
